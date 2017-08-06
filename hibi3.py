543d7d4eee8922f49e7a42710d678dcbda01debb
AkinoKawaii <canarianara@gmail.com>
2017-08-06 16:06:28 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -139,4 +139,4 @@ __________________________________________
                             
 
 print('Starting....')
-client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DFXOqg.VpNyeY4rvjN4Ugg6OFWJdqBkMGU')
+client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DGhtbg.qdzFSqklMQ9LfBRU2fN5mF25S1o')
