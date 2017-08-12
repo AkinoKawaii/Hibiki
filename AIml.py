b5d98e73687e6d40b14dd465133fb74d7187a95c
AkinoKawaii <canarianara@gmail.com>
2017-08-12 22:04:48 +0700

    Update AIml.py

--- AIml.py
+++ AIml.py
@@ -16,9 +16,9 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('how are you'):
-	msg = 'Im excellent'.format(message)
-	await client.send_message(message.channel, msg)
+   if message.content.startswith('how are you'):
+        msg = 'Im good'.format(message)
+        await client.send_message(message.channel, msg)
 
 @client.event
 async def on_message(message):
