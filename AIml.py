8f56033ca458dd85bb760bbf2fbfbee3dffdf59d
AkinoKawaii <canarianara@gmail.com>
2017-08-12 22:18:31 +0700

    Update AIml.py

--- AIml.py
+++ AIml.py
@@ -10,15 +10,9 @@ async def on_message(message):
         msg = 'Hello {0.author.mention}'.format(message)
         await client.send_message(message.channel, msg)
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    elif message.content.startswith('how are you'):
-        msg = 'Im good and you?'.format(message)
-        await client.send_message(message.channel, msg)
+    if message.content.startswith("how are you"):
+        msg = "{}".format(Im good and you?)
+        await client.send_message(message.channel,msg)
 
 @client.event
 async def on_message(message):
