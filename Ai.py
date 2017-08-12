6c67553aaf9ec6fdcedd98da6e29186663fbc2cb
AkinoKawaii <canarianara@gmail.com>
2017-08-12 23:09:36 +0700

    Update Ai.py

--- Ai.py
+++ Ai.py
@@ -3,5 +3,5 @@ import discord
 @client.event
 async def on_message(message)
     if message.content.startswith("how are you"):
-        msg = "{}".format(Im good and you?)
+        msg = "{}".format(Im good and you)
         await client.send_message(message.channel,msg)
