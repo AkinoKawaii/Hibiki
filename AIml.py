a9879aae0532901a1b9bf08954f1e853fdc454c0
AkinoKawaii <canarianara@gmail.com>
2017-08-12 21:58:57 +0700

    Update AIml.py

--- AIml.py
+++ AIml.py
@@ -130,4 +130,11 @@ async def on_message(message):
 	msg = 'what are you laugh for?'.format(message)
 	await client,send_message(message.channel, msg)
 		
-	
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DGhzpQ.1aGHRakWrOz-MMDk6i5m6KsJ7ag')
