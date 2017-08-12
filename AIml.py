87d3f89c28d004d08eec3d3404701aaa01f52213
AkinoKawaii <canarianara@gmail.com>
2017-08-12 22:37:13 +0700

    Update AIml.py

--- AIml.py
+++ AIml.py
@@ -1,15 +1,7 @@
 import discord
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+client = discord.Client()
 
-    if message.content.startswith('hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-	
 @client.event
 async def on_message(message):
     if message.content.startswith('how are you'):
