f71205a7c6a6f936f6be82b2a1481516abe72d1b
AkinoKawaii <canarianara@gmail.com>
2017-08-12 22:55:30 +0700

    Update AIml.py

--- AIml.py
+++ AIml.py
@@ -3,129 +3,74 @@ import discord
 client = discord.Client()
 
 @client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
+async def on_message(message)
+if message.content.startswith('hi'):
+        await client.send_message(message.channel,"Hello")
 
 @client.event
 async def on_message(message):
    if message.content.startswith('how are you'):
-        await client.send_message(message.channel,"Im fine")
+        await client.send_message(message.channel,"Im fine, and you?")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+   if message.content.startswith('Im good'):
+        await client.send_message(message.channel,"Nice")
 
-    if message.content.startswith('Im good'):
-	msg = 'nice'.format(message)
-	await client.send_message(message.channel, msg)
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
     if message.content.startswith('good morning'):
-	msg = 'Good morning'.format(message)
-	await client.send_message(message.channel, msg)
+        await client.send_message(message.channel,"Good morning")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('goodevening'):
-	msg = 'Good evening'.format(message)
-	await client.send_message(message.channel, msg)
+   if message.content.startswith('good evening'):
+        await client.send_message(message.channel,"Good evening")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
     if message.content.startswith('good afternoon'):
-	msg = 'Good afternoon'.format(message)
-	await client.send_message(message.channel, msg)
+        await client.send_message(message.channel,"Good Afternoon")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+   if message.content.startswith('good night'):
+        await client.send_message(message.channel,"Good Night")
 
-    if message.content.startswith('good night'):
-	msg = 'Good night'.format(message)
-	await client.send_message(message.channel, msg)
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('what are you doing'):
-	msg = 'Im always stay online, and playing music'.format(message)
-	await client.send_message(message.channel, msg)
+   if message.content.startswith('what are you doing'):
+        await client.send_message(message.channel,"I always stay online, and always ready to play music")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
     if message.content.startswith('how old are you'):
-	msg = 'Im 15 years old in human age'.format(message)
-	await client.send_message(message.channel, msg)
+        await client.send_message(message.channel,"Im 15 years old in human age")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('who are you')
-	msg = 'Im Hibiki A.I Bot'.format(message)
-	await client.send_message(message.channel, msg)
+  if message.content.startswith('who are you'):
+        await client.send_message(message.channel,"Im Hibiki A.I Bot")
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+   if message.content.startswith('whats your name'):
+        await client.send_message(message.channel,"My name is Hibiki Desu!")
 
-    if message.content.startswith('whats your name')
-	msg = 'my name is Hibiki Desu!'.format(message)
-	await client.send_message(message.channel, msg)
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+   if message.content.startswith('where do you live'):
+        await client.send_message(message.channel,"I live in Internet")
 
-    if message.content.startswith('where do you live')
-	msg = 'Im AI so I live in Internet'.format(message)
-	await client.send_message(message.channel, msg)
 
 @client.event
 async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
+   if message.content.startswith('lol'):
+        await client.send_message(message.channel,"what you are laughing for?")
 
-    if message.content.startswith('lol')
-	msg = 'what are you laugh for?'.format(message)
-	await client,send_message(message.channel, msg)
 		
 @client.event
 async def on_ready():
