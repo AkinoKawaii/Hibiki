0d54d844ebabc33b93f2e0380f11404a1c7d476d
AkinoKawaii <canarianara@gmail.com>
2017-08-16 15:35:04 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -20,42 +20,6 @@ def escape_mass_mentions(text):
     return text
 
 @client.event
-async def on_member_update(before, after):
-        server = after.server
-        member = after
-        if before.name != after.name:
-            embed = discord.Embed(description='From {0.name} ({0.id}) to {1.name}'.format(before, after))
-            embed.set_author(name='{0.mention} Name changed'.format(escape_mass_mentions(str(discord.Member))), icon_url=member.avatar_url)
-            await client.send_message(after.server, embed=embed)
-        if before.nick != after.nick:
-            embed = discord.Embed(description='From {0.nick} ({0.id}) to {1.nick}'.format(before, after))
-            embed.set_author(name='Nickname changed', icon_url=member.avatar_url)
-            await client.send_message(after.server, embed=embed)
-        if before.roles != after.roles:
-            if len(before.roles) > len(after.roles):
-                for role in before.roles:
-                    if role not in after.roles:
-                        embed = discord.Embed(description='{0.display_name} ({0.id}) lost the {1.name} role'.format(before, role))
-                        embed.set_author(name='Role removed', icon_url=member.avatar_url)
-                        await client.send_message(after.server, embed=embed)
-            elif len(before.roles) < len(after.roles):
-                for role in after.roles:
-                    if role not in before.roles:
-                        embed = discord.Embed(description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
-                        embed.set_author(name='Role applied', icon_url=member.avatar_url)
-                        await client.send_message(after.server, embed=embed)
-
-@client.event
-async def on_member_remove(member):
-    server = member.server
-    fmt = '{0.mention} leave the {1.name} server'
-    await client.send_message(server, fmt.format(member, server))
-@client.event
-async def on_member_join(member):
-    server = member.server
-    fmt = 'Welcome {0.mention} to {1.name}!'
-    await client.send_message(server, fmt.format(member, server))
-@client.event
 async def on_ready():
     print('Logged in as')
     print(client.user.name)
@@ -180,6 +144,44 @@ h!disconnect        disconnect from the VC
             text = re.findall('h!say\s(.*)',message.content)
             refined = ' '.join(text)
             await client.send_message(message.channel, '{0}'.format(refined))
+            
+
+@client.event
+async def on_member_update(before, after):
+        server = after.server
+        member = after
+        if before.name != after.name:
+            embed = discord.Embed(description='From {0.name} ({0.id}) to {1.name}'.format(before, after))
+            embed.set_author(name='{0.mention} Name changed'.format(member, server), icon_url=member.avatar_url)
+            await client.send_message(after.server, embed=embed)
+        if before.nick != after.nick:
+            embed = discord.Embed(description='From {0.nick} ({0.id}) to {1.nick}'.format(before, after))
+            embed.set_author(name='Nickname changed', icon_url=member.avatar_url)
+            await client.send_message(after.server, embed=embed)
+        if before.roles != after.roles:
+            if len(before.roles) > len(after.roles):
+                for role in before.roles:
+                    if role not in after.roles:
+                        embed = discord.Embed(description='{0.display_name} ({0.id}) lost the {1.name} role'.format(before, role))
+                        embed.set_author(name='Role removed', icon_url=member.avatar_url)
+                        await client.send_message(after.server, embed=embed)
+            elif len(before.roles) < len(after.roles):
+                for role in after.roles:
+                    if role not in before.roles:
+                        embed = discord.Embed(description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
+                        embed.set_author(name='Role applied', icon_url=member.avatar_url)
+                        await client.send_message(after.server, embed=embed)
+
+@client.event
+async def on_member_remove(member):
+    server = member.server
+    fmt = '{0.mention} leave the {1.name} server'
+    await client.send_message(server, fmt.format(member, server))
+@client.event
+async def on_member_join(member):
+    server = member.server
+    fmt = 'Welcome {0.mention} to {1.name}!'
+    await client.send_message(server, fmt.format(member, server))
       
   
 print('Starting....')
