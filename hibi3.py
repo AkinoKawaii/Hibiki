be09d96acd9b805fa68c1f3c917a70236fe31c72
AkinoKawaii <canarianara@gmail.com>
2017-08-16 15:30:17 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -25,24 +25,24 @@ async def on_member_update(before, after):
         member = after
         if before.name != after.name:
             embed = discord.Embed(description='From {0.name} ({0.id}) to {1.name}'.format(before, after))
-            embed.set_author(name='{0.name} Name changed', icon_url=member.avatar_url)
+            embed.set_author(name='{0.mention} Name changed'.format(escape_mass_mentions(str(discord.Member))), icon_url=member.avatar_url)
             await client.send_message(after.server, embed=embed)
         if before.nick != after.nick:
             embed = discord.Embed(description='From {0.nick} ({0.id}) to {1.nick}'.format(before, after))
-            embed.set_author(name='{0.name} Nickname changed', icon_url=member.avatar_url)
+            embed.set_author(name='Nickname changed', icon_url=member.avatar_url)
             await client.send_message(after.server, embed=embed)
         if before.roles != after.roles:
             if len(before.roles) > len(after.roles):
                 for role in before.roles:
                     if role not in after.roles:
                         embed = discord.Embed(description='{0.display_name} ({0.id}) lost the {1.name} role'.format(before, role))
-                        embed.set_author(name='{0.name} Role removed', icon_url=member.avatar_url)
+                        embed.set_author(name='Role removed', icon_url=member.avatar_url)
                         await client.send_message(after.server, embed=embed)
             elif len(before.roles) < len(after.roles):
                 for role in after.roles:
                     if role not in before.roles:
                         embed = discord.Embed(description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
-                        embed.set_author(name='{0.name} Role applied', icon_url=member.avatar_url)
+                        embed.set_author(name='Role applied', icon_url=member.avatar_url)
                         await client.send_message(after.server, embed=embed)
 
 @client.event
