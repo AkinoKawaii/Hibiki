e3c261688152e37a2e40b524adbd4a59d6534eea
AkinoKawaii <canarianara@gmail.com>
2017-08-16 16:04:09 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -152,11 +152,11 @@ async def on_member_update(before, after):
         member = after
         if before.name != after.name:
             embed = discord.Embed(description='From {0.name} ({0.id}) to {1.name}'.format(before, after))
-            embed.set_author(icon_url=member.avatar_url, name='Name Changed')
+            embed.set_author( name='Name Changed', icon_url=member.avatar_url)
             await client.send_message(after.server, embed=embed)
         if before.nick != after.nick:
             embed = discord.Embed(description='From {0.nick} ({0.id}) to {1.nick}'.format(before, after))
-            embed.set_author(icon_url=member.avatar_url, name='Name Changed')
+            embed.set_author( name='Name Changed', icon_url=member.avatar_url)
             await client.send_message(after.server, embed=embed)
         if before.roles != after.roles:
             if len(before.roles) > len(after.roles):
