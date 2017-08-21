a91e9f0123e730a1020bf1c1af445b093862f97f
AkinoKawaii <canarianara@gmail.com>
2017-08-21 19:51:45 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -31,9 +31,9 @@ async def on_message(message):
  #   if message.content.startswith('=ok'):
      #   await client.send_message(message.channel,'ok')
     if message.content.startswith('h!orders'):
-    embed = discord.Embed("INFO h!user h!user <tag> h!say <words> MUSIC h!summon h!play <song/url> h!queue h!pause h!resume h!skip h!shuffle h!disconnect")  
-    embed.set_image(url = "https://cdn.discordapp.com/attachments/333240041302261761/349152395449860097/Hibi_Support.jpg")
-    await client.send_message(message.channel, embed = embed_thing)
+    embed = discord.Embed(discription = 'INFO h!user h!user <tag> h!say <words> MUSIC h!summon h!play <song/url> h!queue h!pause h!resume h!skip h!shuffle h!disconnect')  
+    embed.set_image(url = 'https://cdn.discordapp.com/attachments/333240041302261761/349152395449860097/Hibi_Support.jpg')
+    await client.send_message(message.channel, embed=embed )
       
     elif message.content.startswith('h!user'):
         try:
