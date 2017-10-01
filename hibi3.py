19ee3b151c3bacbf6e360f406a13648e3d3a69ea
AkinoKawaii <canarianara@gmail.com>
2017-10-01 11:45:51 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -190,4 +190,5 @@ async def on_message(message):
         elif message.centent.startswith('Invite'):
             await cliet.send_message(message.channel,"<https://cdn.discordapp.com/attachments/333240041302261761/349152395449860097/Hibi_Support.jpg>"
 
+    print('starting...')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DGhzpQ.1aGHRakWrOz-MMDk6i5m6KsJ7ag')
