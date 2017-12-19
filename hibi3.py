70172bd499da31058ef42ce9a114f66235c63452
AkinoKawaii <canarianara@gmail.com>
2017-12-19 20:45:38 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -147,6 +147,12 @@ async def on_member_update(before, after):
                         embed = discord.Embed(description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
                         embed.set_author(name='Role applied', icon_url=member.avatar_url)
                         await client.send_message(client.get_channel('388834987253825537'), after.server, embed=embed)
+@client.event
+async def on_message(message):
+    if message.content.startswith('cmd'):
+        embed = discord.Embed(title='commands', colour=#5be783)
+        embed.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg")
+        await client.send_message(message.channel, embed=embed)
     
 print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRpmzg.lURkk8yVYnsEGS7SpdeGDM3sN0E')
