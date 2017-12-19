b27514080b9f66da8b4aa1b9866d30cd56d63bc4
AkinoKawaii <canarianara@gmail.com>
2017-12-19 20:44:57 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -131,12 +131,6 @@ async def on_message(message):
             refined = ' '.join(text)
             await client.send_message(message.channel, '{0}'.format(refined))
 @client.event
-async def on_message(message):
-    if message.content.startswith('cmd'):
-        embed = discord.Embed(title='commands', colour=#5be783)
-        embed.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg")
-        await client.send_message(message.channel, embed=embed)
-@client.event
 async def on_member_update(before, after):
         server = after.server
         member = after
