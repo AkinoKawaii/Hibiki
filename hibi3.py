bda1a1c3d7ea05ff7d7ba5c00b092de27808933d
AkinoKawaii <canarianara@gmail.com>
2017-12-19 18:26:07 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -24,6 +24,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence( game=discord.Game( name="Show | Invite", type = 1))
     
 @client.event
 async def on_message(message):
