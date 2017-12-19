44505be66005c3e3d43d3039541b8283ea52747a
AkinoKawaii <canarianara@gmail.com>
2017-12-19 20:09:01 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -151,7 +151,7 @@ async def on_member_update(before, after):
 @client.event
 async def on_message(message):
     if message.content.startswith('cmd'):
-        embed = discord.Embed('How to use? type cmd!')
+        embed = discord.Embed('type cmdplus')
         embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg')
         await client.send_message(message.channel, embed=embed)
     
