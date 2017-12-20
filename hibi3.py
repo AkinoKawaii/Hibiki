6393449993e7982010020002433902da3cab2e90
AkinoKawaii <canarianara@gmail.com>
2017-12-20 10:05:02 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -147,13 +147,12 @@ async def on_member_update(before, after):
                         embed = discord.Embed(description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
                         embed.set_author(name='Role applied', icon_url=member.avatar_url)
                         await client.send_message(client.get_channel('388834987253825537'), after.server, embed=embed)
-                        
 @client.event
 async def on_message(message):
     if message.content.startswith('cmd'):
-        em=discord.Embed(description='user say Invite Hibiki Music', colour=#4eeb9b)
-        em.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibikiprofiile.png")
-                         await client.send_message(message.channel,embed=em)
+        em=discord.Embed(description='user say Invite Music Hibiki', colour=#4eeb9b)
+        em.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg")
+        await client.send_message(message.channel,embed=em)                        
                          
 print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRs-OA.inWxGr07ZiJmOTVsAvMKiuYyRwU')
