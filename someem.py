8d91753b4c7269af70de2d58d9401474a5fb7698
AkinoKawaii <canarianara@gmail.com>
2017-12-20 07:46:15 +0700

    Update someem.py

--- someem.py
+++ someem.py
@@ -5,4 +5,4 @@ async def on_message(message):
     if message.content.startswith('cmd'):
         em=discord.Embed(description='commands')
         embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg')
-            await client.send_message(message.channel,embed=em)
+        await client.send_message(message.channel,embed=em)
