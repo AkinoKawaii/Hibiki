3d6c0dc40c127241cf5873bd02cd8dd382aadc53
AkinoKawaii <canarianara@gmail.com>
2017-12-20 12:23:43 +0700

    Update run.py

--- run.py
+++ run.py
@@ -23,6 +23,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence( game=discord.Game( name="cmd | myanimelist.net", type = 1))
     
 @client.event
 async def on_message(message):
