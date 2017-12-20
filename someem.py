317f7db9164833d9ef68d1f0524426f56c11b4f2
AkinoKawaii <canarianara@gmail.com>
2017-12-20 07:36:23 +0700

    Create someem.py

---
+++ someem.py
@@ -0,0 +1,8 @@
+import re
+
+@client.event
+async def on_message(message):
+if message.content.startwith('cmd')
+    em=discord.Embed(description='commands')
+    embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg')
+    await.client.send_message(message.channel, embed=em)
