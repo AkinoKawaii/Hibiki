f9c9d88ae7df540257261e7bd1780f7ec1070398
AkinoKawaii <canarianara@gmail.com>
2017-12-20 07:56:09 +0700

    Update someem.py

--- someem.py
+++ someem.py
@@ -6,6 +6,11 @@ client = discord.Client()
 @client.event
 async def on_message(message):
     if message.content.startswith('cmd'):
-        em=discord.Embed(description='commands')
+        em=discord.Embed(description='test')
         embed.set_author(name='test')
         await client.send_message(message.channel,embed=em)
+        
+@client.event
+async def on_messsage(message):
+    if message.content.startswith('null'):
+        await.client.send_message(message.channel, '0')
