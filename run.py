78d9e56f6a28ad038aa9b04eee310cd21aa272f9
AkinoKawaii <canarianara@gmail.com>
2017-12-20 12:19:22 +0700

    Update run.py

--- run.py
+++ run.py
@@ -121,6 +121,28 @@ async def on_message(message):
             
     elif message.content.startswith('Invite'):
             await client.send_message(message.channel,"https://discordapp.com/oauth2/authorize?client_id=333081212065284096&scope=bot&permissions=506522792")
+            
+    elif message.content.startswith('cmd'):
+            embed=discord.Embed(color=0x00ce5c)
+            embed.set_author(name='Command List',icon_url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibiki-2.png')
+            embed.add_field(name='user', value='User info', inline=False)
+            embed.add_field(name='say', value='Repeat your words', inline=False)
+            embed.add_field(name='Invite', value='Send Invite(bot) link', inline=False)
+            embed.add_field(name='Hibiki', value='Talk with hibiki', inline=False)
+            embed.add_field(name='hsummon', value='Hibiki will connected in your #VoiceChannel', inline=False)
+            embed.add_field(name='hplay', value='Request music', inline=False)
+            embed.add_field(name='hnp', value='Show playing music', inline=False)
+            embed.add_field(name='hqueue', value='Show the queue', inline=False)
+            embed.add_field(name='hshuffle', value='Shuffle the song', inline=False)
+            embed.add_field(name='hpause', value='pause the song', inline=False)
+            embed.add_field(name='hresume', value='resume the paused song', inline=False)
+            embed.add_field(name='hskip', value='to skip song', inline=False)
+            embed.add_field(name='hvolume', value='set the volume (5~100)', inline=False)
+            embed.add_field(name='hsearch', value='searching the song #YouTube', inline=False)
+            embed.add_field(name='hdisconnect', value='disconnect Hibiki from #VoiceChannel', inline=False)
+            embed.set_footer(text='made by OnikaStudio')
+            embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png')
+            await client.send_message(message.channel,embed=embed)
                   
     elif message.content.startswith('say'):
         if message.content == 'say':return
@@ -146,29 +168,7 @@ async def on_member_update(before, after):
                         embed = discord.Embed(color=0x00ce5c, description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
                         embed.set_author(name='Role applied', icon_url=member.avatar_url)
                         await client.send_message(client.get_channel('388834987253825537'), after.server, embed=embed)
-@client.event
-async def on_message(message):
-    if message.content.startswith('cmd'):
-        embed=discord.Embed(color=0x00ce5c)
-        embed.set_author(name='Command List',icon_url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibiki-2.png')
-        embed.add_field(name='user', value='User info', inline=False)
-        embed.add_field(name='say', value='Repeat your words', inline=False)
-        embed.add_field(name='Invite', value='Send Invite(bot) link', inline=False)
-        embed.add_field(name='Hibiki', value='Talk with hibiki', inline=False)
-        embed.add_field(name='hsummon', value='Hibiki will connected in your #VoiceChannel', inline=False)
-        embed.add_field(name='hplay', value='Request music', inline=False)
-        embed.add_field(name='hnp', value='Show playing music', inline=False)
-        embed.add_field(name='hqueue', value='Show the queue', inline=False)
-        embed.add_field(name='hshuffle', value='Shuffle the song', inline=False)
-        embed.add_field(name='hpause', value='pause the song', inline=False)
-        embed.add_field(name='hresume', value='resume the paused song', inline=False)
-        embed.add_field(name='hskip', value='to skip song', inline=False)
-        embed.add_field(name='hvolume', value='set the volume (5~100)', inline=False)
-        embed.add_field(name='hsearch', value='searching the song #YouTube', inline=False)
-        embed.add_field(name='hdisconnect', value='disconnect Hibiki from #VoiceChannel', inline=False)
-        embed.set_footer(text='made by OnikaStudio')
-        embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png')
-        await client.send_message(message.channel,embed=embed)                        
+                           
 print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRs-OA.inWxGr07ZiJmOTVsAvMKiuYyRwU')
 
