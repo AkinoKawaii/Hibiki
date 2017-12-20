68cea1ec021b54f8da962ee6e4df1551edb40928
AkinoKawaii <canarianara@gmail.com>
2017-12-20 12:07:30 +0700

    Update run.py

--- run.py
+++ run.py
@@ -10,6 +10,153 @@ async def on_ready():
     print(client.user.id)
     print('------')
     
+@client.event
+async def on_message(message):
+    if message.content.startswith('cmd'):
+        embed=discord.Embed(color=0x00ce5c)
+        embed.set_author(name='Command List',icon_url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibiki-2.png')
+        embed.add_field(name='user', value='User info', inline=False)
+        embed.add_field(name='say', value='Repeat your words', inline=False)
+        embed.add_field(name='Invite', value='Send Invite(bot) link', inline=False)
+        embed.add_field(name='Hibiki', value='Talk with hibiki', inline=False)
+        embed.add_field(name='hsummon', value='Hibiki will connected in your #VoiceChannel', inline=False)
+        embed.add_field(name='hplay', value='Request music', inline=False)
+        embed.add_field(name='hnp', value='Show playing music', inline=False)
+        embed.add_field(name='hqueue', value='Show the queue', inline=False)
+        embed.add_field(name='hshuffle', value='Shuffle the song', inline=False)
+        embed.add_field(name='hpause', value='pause the song', inline=False)
+        embed.add_field(name='hresume', value='resume the paused song', inline=False)
+        embed.add_field(name='hskip', value='to skip song', inline=False)
+        embed.add_field(name='hvolume', value='set the volume (5~100)', inline=False)
+        embed.add_field(name='hsearch', value='searching the song #YouTube', inline=False)
+        embed.add_field(name='hdisconnect', value='disconnect Hibiki from #VoiceChannel', inline=False)
+        embed.set_footer(text='made by OnikaStudio')
+        embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png')
+        await client.send_message(message.channel,embed=embed)
+        
+@client.event
+async def on_message(message):
+    random_color = colors()
+ #   if message.content.startswith('=ok'):
+     #   await client.send_message(message.channel,'ok')
+    if message.content.startswith('Show'):
+        await client.send_message(message.channel,"http://bit.ly/2zwECwH http://bit.ly/2AogxWd")
+    elif message.content.startswith('user'):
+        try:
+            if message.channel.id !='274387797140570112':
+                coloursman = int(random_color['color'])
+                if message.content == 'user':
+                    discord.Member = None
+                    author = message.author
+                    if not discord.Member :
+                        discord.Member = message.author
+                    roles = [x.name for x in discord.Member.roles if x.name != "@everyone"]
+                    if not roles: roles = ["None"]
+                    data = "\n"
+                    data += "Name: {}\n".format(escape_mass_mentions(str(discord.Member)))
+                    data += "Nickname: {0}\n".format(discord.Member.display_name)
+                    data += "ID: {}\n".format(discord.Member.id)
+                    passed = (message.timestamp - discord.Member.created_at).days
+                    data += "Created: {} days ago.\n".format(passed)
+                    passed = (message.timestamp - discord.Member.joined_at).days
+                    data += "Joined: {} days ago\n".format(passed)
+                    data += "Server: {}\n".format(discord.Member.server)
+                    data += "Status: {}\n".format(discord.Member.status)
+                    data += "Playing: {}\n".format(discord.Member.game)
+                    data += "Bot Account: {}\n".format(discord.Member.bot)
+                    data += "Roles: {}\t".format(", ".join(roles))
+                    data += "**({0})**\n".format(len(roles))
+                    data += "Top Role: {}\n\n".format(discord.Member.top_role)
+                
+                    user_em = discord.Embed(
+                    description=data)
+                    user_em.set_author(
+                    name="Your Status :")
+                    user_em.set_image(url = discord.Member.avatar_url)
+                    user_em.color=coloursman
+                
+                    await client.send_message(message.channel,embed = user_em)          
+           
+           
+           
+                else:
+                    check_tagger = re.findall('user\s(.*)',message.content)
+        
+                    hilol_ = ' '.join(check_tagger)
+                    no1is = re.sub(r'[^\w]', ' ', hilol_)
+                    thisiscool = no1is.replace(' ','')
+                
+                    member_id = message.author.server.get_member(user_id = thisiscool)
+                    
+                    discord.Member = member_id 
+                    roles = [x.name for x in discord.Member.roles if x.name != "@everyone"]
+                    if not roles: roles = ["None"]
+                    data = "\n"
+                    data += "Name: {}\n".format(escape_mass_mentions(str(discord.Member)))
+                    data += "ID: {}\n".format(discord.Member.id)
+                    passed = (message.timestamp - discord.Member.created_at).days
+                    data += "Created: {} days ago\n".format(passed)
+                    passed = (message.timestamp - discord.Member.joined_at).days
+                    data += "Joined: {} days ago\n".format(passed)
+                    data += "Server: {}\n".format(discord.Member.server)
+                    data += "Status: {}\n".format(discord.Member.status)
+                    data += "Playing: {}\n".format(discord.Member.game)
+                    data += "Bot Account: {}\n".format(discord.Member.bot)
+                    data += "Roles: {}\t".format(", ".join(roles))
+                    data += "**({0})**\n".format(len(roles))
+                    data += "Top Role: {}\n\n".format(discord.Member.top_role)
+                    user_em = discord.Embed(
+                    description=data)
+                    user_em.set_author(
+                    name="User Status :")
+                    user_em.set_image(url = discord.Member.avatar_url)
+                    user_em.color=coloursman
+                
+                    await client.send_message(message.channel,embed = user_em)
+  
+        except Exception as e: print (str(e))
+    
+    
+    elif message.content.startswith('Hibiki'):  
+        if message.content == 'Hibiki':return
+        else:
+            text = re.findall('Hibiki\s(.*)',message.content)
+            refined = ' '.join(text)
+            near_done = refined
+            lower_them = near_done.lower()
+            stuff = lower_them
+            new = talking.talk(stuff)
+            await client.send_typing(message.channel)
+            await client.send_message(message.channel,new)
+            
+    elif message.content.startswith('Invite'):
+            await client.send_message(message.channel,"https://discordapp.com/oauth2/authorize?client_id=333081212065284096&scope=bot&permissions=506522792")
+                  
+    elif message.content.startswith('say'):
+        if message.content == 'say':return
+        else:
+            text = re.findall('say\s(.*)',message.content)
+            refined = ' '.join(text)
+            await client.send_message(message.channel, '{0}'.format(refined))
+        
+@client.event
+async def on_member_update(before, after):
+        server = after.server
+        member = after
+        if before.roles != after.roles:
+            if len(before.roles) > len(after.roles):
+                for role in before.roles:
+                    if role not in after.roles:
+                        embed = discord.Embed(color=0x00ce5c, description='{0.display_name} ({0.id}) lost the {1.name} role'.format(before, role))
+                        embed.set_author(name='Role removed', icon_url=member.avatar_url)
+                        await client.send_message(client.get_channel('388834987253825537'), after.server, embed=embed)
+            elif len(before.roles) < len(after.roles):
+                for role in after.roles:
+                    if role not in before.roles:
+                        embed = discord.Embed(color=0x00ce5c, description='{0.display_name} ({0.id}) got the {1.name} role'.format(before, role))
+                        embed.set_author(name='Role applied', icon_url=member.avatar_url)
+                        await client.send_message(client.get_channel('388834987253825537'), after.server, embed=embed)
+                        
 print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRs-OA.inWxGr07ZiJmOTVsAvMKiuYyRwU')
 
