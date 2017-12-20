ee56d92ba0233683cbb0f96be95a998f7c362948
AkinoKawaii <canarianara@gmail.com>
2017-12-20 11:35:51 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -28,6 +28,30 @@ async def on_ready():
     
 @client.event
 async def on_message(message):
+    if message.content.startswith('cmd'):
+        embed=discord.Embed(color=0x00ce5c)
+        embed.set_author(name='Command List',icon_url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibiki-2.png')
+        embed.add_field(name='user', value='User info', inline=False)
+        embed.add_field(name='say', value='Repeat your words', inline=False)
+        embed.add_field(name='Invite', value='Send Invite(bot) link', inline=False)
+        embed.add_field(name='Hibiki', value='Talk with hibiki', inline=False)
+        embed.add_field(name='hsummon', value='Hibiki will connected in your #VoiceChannel', inline=False)
+        embed.add_field(name='hplay', value='Request music', inline=False)
+        embed.add_field(name='hnp', value='Show playing music', inline=False)
+        embed.add_field(name='hqueue', value='Show the queue', inline=False)
+        embed.add_field(name='hshuffle', value='Shuffle the song', inline=False)
+        embed.add_field(name='hpause', value='pause the song', inline=False)
+        embed.add_field(name='hresume', value='resume the paused song', inline=False)
+        embed.add_field(name='hskip', value='to skip song', inline=False)
+        embed.add_field(name='hvolume', value='set the volume (5~100)', inline=False)
+        embed.add_field(name='hsearch', value='searching the song #YouTube', inline=False)
+        embed.add_field(name='hdisconnect', value='disconnect Hibiki from #VoiceChannel', inline=False)
+        embed.set_footer(text='made by OnikaStudio')
+        embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png')
+        await client.send_message(message.channel,embed=embed)
+    
+@client.event
+async def on_message(message):
     random_color = colors()
  #   if message.content.startswith('=ok'):
      #   await client.send_message(message.channel,'ok')
@@ -130,30 +154,6 @@ async def on_message(message):
             text = re.findall('say\s(.*)',message.content)
             refined = ' '.join(text)
             await client.send_message(message.channel, '{0}'.format(refined))
-            
-@client.event
-async def on_message(message):
-    if message.content.startswith('cmd'):
-        embed=discord.Embed(color=0x00ce5c)
-        embed.set_author(name='Command List',icon_url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibiki-2.png')
-        embed.add_field(name='user', value='User info', inline=False)
-        embed.add_field(name='say', value='Repeat your words', inline=False)
-        embed.add_field(name='Invite', value='Send Invite(bot) link', inline=False)
-        embed.add_field(name='Hibiki', value='Talk with hibiki', inline=False)
-        embed.add_field(name='hsummon', value='Hibiki will connected in your #VoiceChannel', inline=False)
-        embed.add_field(name='hplay', value='Request music', inline=False)
-        embed.add_field(name='hnp', value='Show playing music', inline=False)
-        embed.add_field(name='hqueue', value='Show the queue', inline=False)
-        embed.add_field(name='hshuffle', value='Shuffle the song', inline=False)
-        embed.add_field(name='hpause', value='pause the song', inline=False)
-        embed.add_field(name='hresume', value='resume the paused song', inline=False)
-        embed.add_field(name='hskip', value='to skip song', inline=False)
-        embed.add_field(name='hvolume', value='set the volume (5~100)', inline=False)
-        embed.add_field(name='hsearch', value='searching the song #YouTube', inline=False)
-        embed.add_field(name='hdisconnect', value='disconnect Hibiki from #VoiceChannel', inline=False)
-        embed.set_footer(text='made by OnikaStudio')
-        embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png')
-        await client.send_message(message.channel,embed=embed)
         
 @client.event
 async def on_member_update(before, after):
