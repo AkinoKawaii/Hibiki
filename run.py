da7f28e6b13c81623776a8749c580a29dbb7074c
AkinoKawaii <canarianara@gmail.com>
2017-12-20 12:05:21 +0700

    Update run.py

--- run.py
+++ run.py
@@ -3,5 +3,13 @@ import asyncio
 import hibi3
 import someem
 client = discord.Client()
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    
+print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRs-OA.inWxGr07ZiJmOTVsAvMKiuYyRwU')
 
