29a7b59f06b065b10d95314b5ca66eec5807c758
AkinoKawaii <canarianara@gmail.com>
2017-12-20 08:27:06 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -153,14 +153,7 @@ async def on_message(message):
     if message.content.startswith('cmd'):
         em=discord.Embed(description='command')
         em.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg")
-        await client.send_message(message.channel,embed=em)                         
-                        
-@client.event
-async def on_message(message):
-    if message.content.startswith('.test'):
-        em=discord.Embed(description='test')
-        em.set_author(name='test')
-        await client.send_message(message.channel,embed=em)                        
+        await client.send_message(message.channel,embed=em)                                                
 
 print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRs-OA.inWxGr07ZiJmOTVsAvMKiuYyRwU')
