8b1eb6284c8ae1639cf76e4d53c65f2007aebb54
AkinoKawaii <canarianara@gmail.com>
2017-12-20 07:45:20 +0700

    Update someem.py

--- someem.py
+++ someem.py
@@ -3,6 +3,6 @@ import re
 @client.event
 async def on_message(message):
     if message.content.startswith('cmd'):
-    em=discord.Embed(description='commands')
-    embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg')
-    await client.send_message(message.channel,embed=em)
+        em=discord.Embed(description='commands')
+        embed.set_image(url='https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/command.jpeg')
+            await client.send_message(message.channel,embed=em)
