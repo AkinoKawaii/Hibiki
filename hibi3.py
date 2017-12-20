39c0b70f63f97b50bc68f6b08e2d7ff06674a6e3
AkinoKawaii <canarianara@gmail.com>
2017-12-20 09:48:11 +0700

    Update hibi3.py

--- hibi3.py
+++ hibi3.py
@@ -155,10 +155,10 @@ async def on_message(message):
         em.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/hibikiprofiile.png")
         await client.send_message(message.channel,embed=em)
                          
-    elif message.content.startswith('Music'):
-        em=discord.Embed(tittle='music commands', description='hsummon, hplay, hpause, hresume, hskip, hvolume, hsearch, hshuffle, hdisconnect', colour=#4eeb9b)
-        em.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png")
-        await client.send_message(message.channel,embed=em)                          
+        elif message.content.startswith('Music'):
+                em=discord.Embed(tittle='music commands', description='hsummon, hplay, hpause, hresume, hskip, hvolume, hsearch, hshuffle, hdisconnect', colour=#4eeb9b)
+                em.set_image(url="https://raw.githubusercontent.com/AkinoKawaii/Hibiki/master/music.png")
+                await client.send_message(message.channel,embed=em)                          
 
 print('Starting....')
 client.run('MzMzMDgxMjEyMDY1Mjg0MDk2.DRs-OA.inWxGr07ZiJmOTVsAvMKiuYyRwU')
